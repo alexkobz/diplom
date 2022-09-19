883d4f5082eb52383688a149a2f0b27f44fd3772
kobzaale <alexander.kobzar@externals.adidas.com>
2022-09-19 21:48:03 +0300

    gd

--- gd_api.py
+++ gd_api.py
@@ -1,9 +1,11 @@
+import os
+import csv
 import requests
 from bs4 import BeautifulSoup
 
 
-def api(nodes):
-    path = r'C:\Users\kobzaale\Desktop\diplom'
+def api(nodes, path):
+
     for node in nodes:
 
         url = f'http://transcript.duma.gov.ru/node/{str(node)}/'
@@ -21,7 +23,35 @@ def api(nodes):
                 file.write(transcript)
 
 
+def count(path):
+
+    democracy = 'демократ'
+
+    for root, dirs, files in os.walk(path, topdown=False):
+        for file in files:
+
+            if file[:-4] == '.csv':
+                continue
+            counter = 0
+            with open(root + '\\' + file, 'r') as txt:
+                transcript = txt.read()
+            transcript_list = transcript.split()
+
+            for word in transcript_list:
+                if democracy in word:
+                    counter += 1
+
+            if counter:
+                with open(root + '\\' + 'count.csv', 'a+', encoding='utf8', newline='') as csv_file:
+                    writer = csv.writer(csv_file, delimiter=';')
+                    row = (file, counter)
+                    writer.writerow(row)
+
+
 def main():
+
+    basefolder = r'C:\Users\kobzaale\Desktop\diplom'
+
     # 18 января 2000 г. - 22 октября 2010 г.
     nodes_reverse = range(2265, 43, -1)
 
@@ -29,12 +59,17 @@ def main():
     nodes = range(3332, 3341)
 
     try:
-        api(nodes_reverse)
-        api(nodes)
+        api(nodes_reverse, basefolder)
+        api(nodes, basefolder)
     except ConnectionError:
         print('ConnectionError')
     except AttributeError:
         print('AttributeError')
+
+    try:
+        count(basefolder)
+    except FileNotFoundError:
+        print('FileNotFoundError')
 
 
 if __name__ == '__main__':
