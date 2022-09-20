33f92b85ebf4e027a31773464ea97a11050d37f9
kobzaale <alexander.kobzar@externals.adidas.com>
2022-09-20 14:20:12 +0300

    gd

--- gd_api.py
+++ gd_api.py
@@ -16,11 +16,21 @@ def api(nodes, path):
         parsed_html = BeautifulSoup(html, 'lxml')
         header = parsed_html.body.find('div', attrs={'class': 'header-bord'}).text.replace('\n', ' ')
         header_list = header.split()
+        header_list = header_list[2:-4]
+        header_list = header_list[:3]
         if header_list[0] == 'Стенограмма':
-            header = ' '.join(header_list[2:-4])
+            header = ' '.join(header_list)
             transcript = parsed_html.body.find('div', attrs={'id': 'selectable-content'}).text
             with open(path + '\\' + str(header) + '.txt', 'w') as file:
                 file.write(transcript)
+
+        duma = os.path.join(path, 'gd')
+        os.mkdir(duma)
+        for f in range(3, 8):
+            convocation = os.path.join(duma, str(f))
+            os.mkdir(convocation)
+            convocation_archive = os.path.join(convocation, 'archive')
+            os.mkdir(convocation_archive)
 
 
 def count(path):
@@ -32,23 +42,25 @@ def count(path):
             f = open(root + '\\' + 'count.csv', 'w')
             f.close()
         for file in files:
-
-            if file[:-4] == '.csv':
+            if file[-4:] == '.csv':
                 continue
+
             counter = 0
             with open(root + '\\' + file, 'r') as txt:
                 transcript = txt.read()
-            transcript_list = transcript.split()
+            transcript_list = transcript.lower().split()
 
             for word in transcript_list:
-                if democracy in word:
+                if democracy in word and 'либерально-демократ' not in word:
                     counter += 1
 
             if counter:
                 with open(root + '\\' + 'count.csv', 'a+', encoding='utf8', newline='') as csv_file:
                     writer = csv.writer(csv_file, delimiter=';')
-                    row = (file, counter)
+                    row = (file[:-4], counter)
                     writer.writerow(row)
+            else:
+                os.rename(root + '\\' + file, root + '\\' + 'archive\\' + file)
 
 
 def main():
@@ -68,6 +80,16 @@ def main():
         print('ConnectionError')
     except AttributeError:
         print('AttributeError')
+
+    #
+    # Manually sort txt files by convocations. Then run code below
+    #
+    # 3rd convocation: 2000 - 2003
+    # 4th convocation: 2004 - 2007
+    # 5th convocation: 2008 - 2011
+    # 6th convocation: 2012 - 2016
+    # 7th convocation: 2017 - 2021
+    #
 
     try:
         count(basefolder)
