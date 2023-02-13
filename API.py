db02a64b27f8d6fad5ac3c840c01e772e9158aba
alexkobz <alexanderkobzarrr@gmail.com>
2023-02-14 00:18:17 +0300

    API and SQL

---
+++ API.py
@@ -0,0 +1,28 @@
+# This Python file uses the following encoding: utf-8
+
+from bs4 import BeautifulSoup
+from requests import get
+
+
+class API:
+    def __init__(self, url, headers=''):
+        self._url = url
+        self._headers = headers
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def headers(self):
+        return self._headers
+
+    @staticmethod
+    def _api_get(url=url.__str__(), headers=headers.__str__()) -> BeautifulSoup:
+        try:
+            response = get(url, headers=headers)
+            html = response.text
+            parsed_html = BeautifulSoup(html, 'html.parser')
+            return parsed_html
+        except Exception as e:
+            print(e.__str__())
